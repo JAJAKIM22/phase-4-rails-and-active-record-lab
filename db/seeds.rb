9dbfaa25d5794577cb2f436df98aeb11da7894dd
Peter Kimani <peterjajakim@gmail.com>
2022-09-27 09:45:51 +0300

    LAB COMPLETE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: 'Peter', last_name: 'Kimani', grade: 100)
+Student.create!(first_name: 'Susan', last_name: 'Kihika', grade: 90)
